e4c453d441d7067fefc4496c1264d76c66cec216
Adriel Cavalcante <adriel.cavalcante2004@gmail.com>
2021-10-30 18:05:26 -0300

    Fix Web
    Remove the typicode link when testing locally

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000' || 'https://my-json-server.typicode.com/AdrielCavalcante/costs'
+    baseURL: 'https://my-json-server.typicode.com/AdrielCavalcante/costs' || 'http://localhost:8000'
 });
 
 export default api;
